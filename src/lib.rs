edc1c7e9c7aa0a3618ddf49ef76f44c9cbcac80a
Canop <cano.petrole@gmail.com>
2021-08-09 17:46:32 +0200

    Better behavior on non compatible OS or terminals

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,33 @@
 /*!
 This crate answers the question *"Is the terminal dark or light?"*.
 
-It provides either
+It provides
 
 * the background color as RGB
 * the background color's luma, which varies from 0 (black) to 1 (white)
 
-A use case in a TUI is to determine what set of colors would be more suitable depending on the terminal's background:
+A use case in a TUI is to determine what set of colors would be most suitable depending on the terminal's background:
 ```
 let should_use_light_skin = terminal_light::luma()
     .map_or(false, |luma| luma > 0.6);
 ```
 
+The behavior and result of the [bg_color] and [luma] functions depend:
+
+On non unix-like platforms, you'll receive a [TlError::Unsupported] error (help welcome to improve this).
+
+On unix-like (linux, Darwin, etc.), a terminal sequence is sent to stdout and the response is read (with timeout) on stdin.
+
+If the terminal is somehow modern, the answer is received and analyzed to give you the color.
+
+If the terminal doesn't answer fast enough, a [TlError::Timeout] error is returned (current timeout is 20ms).
+
+If the terminal's answer isn't understood, an other type of error is returned.
+
 */
 
 use {
+    nix::sys::epoll::*,
     std::io::{self, Read, Write},
 };
 
@@ -28,29 +41,44 @@ pub enum TlError {
     #[error("Parse Int error: {0}")]
     ParseInt(#[from] std::num::ParseIntError),
     #[error("Wrong answer format: {0}")]
-    WrongFormat(String)
+    WrongFormat(String),
+    #[error("Timeout waiting for xterm")]
+    Timeout,
+    #[error("Terminal error code: {0}")]
+    TerminalError(i64),
+    #[error("Nix error: {0}")]
+    NixError(#[from] nix::errno::Errno),
+    #[error("Unsupported platform")] // not unix-like
+    Unsupported,
 }
 
 /// RGB color, with u16 components
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Rgb {
+    /// red
     pub r: u16,
+    /// green
     pub g: u16,
+    /// blue
     pub b: u16,
 }
 
 impl Rgb {
+    /// Create a new RGB color from its components
     pub fn new(r: u16, g: u16, b: u16) -> Self {
         Self { r, g, b }
     }
+    /// red part in `[0,1]`
     pub fn rp(self) -> f32 {
-        self.r as f32 / 65536f32
+        self.r as f32 / 65535f32
     }
+    /// green part in `[0,1]`
     pub fn gp(self) -> f32 {
-        self.g as f32 / 65536f32
+        self.g as f32 / 65535f32
     }
+    /// blue part in `[0,1]`
     pub fn bp(self) -> f32 {
-        self.b as f32 / 65536f32
+        self.b as f32 / 65535f32
     }
     /// Compute the Luma value characterizing the "light" of the color,
     /// going from 0 (black) to 1 (white).
@@ -61,20 +89,45 @@ impl Rgb {
     }
 }
 
-/// Query the bg color, assuming the terminal is in raw mode
-fn query_bg_color() -> Result<Rgb, TlError> {
-    // we use the "dynamic colors" OSC escape sequence. It's sent with a ? for
-    // a query and normally answered by the terminal with a color.
-    // Reference: https://stackoverflow.com/a/28334701/263525
-    let query = "\x1b]11;?\x07";
+#[cfg(not(unix))]
+fn query_xterm(query: &str, response: &mut[u8], timeout_ms: isize) -> Result<usize, TlError> {
+    Err(TlError::Unsupported)
+}
+
+#[cfg(unix)]
+fn query_xterm(query: &str, response: &mut[u8], timeout_ms: isize) -> Result<usize, TlError> {
     let stdin = io::stdin();
     let mut stdin = stdin.lock();
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
     write!(stdout, "{}", query)?;
     stdout.flush()?;
+    let poll_fd = epoll_create1(EpollCreateFlags::empty())?;
+    let mut event = EpollEvent::new(EpollFlags::EPOLLIN, 0);
+    epoll_ctl(
+        poll_fd,
+        EpollOp::EpollCtlAdd,
+        0, // STDIN_FILENO
+        Some(& mut event),
+    )?;
+    let mut events = [EpollEvent::empty(); 1];
+    let n = epoll_wait(poll_fd, &mut events, timeout_ms)?;
+    if n == 0 {
+        Err(TlError::Timeout)
+    } else {
+        let n = stdin.read(&mut response[..])?;
+        Ok(n)
+    }
+}
+
+/// Query the bg color, assuming the terminal is in raw mode
+fn query_bg_color() -> Result<Rgb, TlError> {
+    // we use the "dynamic colors" OSC escape sequence. It's sent with a ? for
+    // a query and normally answered by the terminal with a color.
+    // Reference: https://stackoverflow.com/a/28334701/263525
+    let query = "\x1b]11;?\x07";
     let mut buffer = [0;50];
-    let n = stdin.read(&mut buffer[..])?;
+    let n = query_xterm(query, &mut buffer, 20)?;
     let s = std::str::from_utf8(&buffer[..n])?;
     match s.strip_prefix("\x1b]11;rgb:") {
         Some(raw_color) if raw_color.len() >= 14 => {
@@ -116,3 +169,4 @@ pub fn luma() -> Result<f32, TlError> {
     crossterm::terminal::disable_raw_mode()?;
     bg_color.map(|c| c.luma())
 }
+
