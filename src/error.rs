74499f50799b09dc6084504a629a64612e3f0cfa
Canop <cano.petrole@gmail.com>
2022-01-03 22:21:10 +0100

    move xtem querying into a separate crate for reuse
    new crate: xterm-query

--- src/error.rs
+++ src/error.rs
@@ -4,21 +4,12 @@
 pub enum TlError {
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
-    #[error("UTF8 error: {0}")]
-    Utf8(#[from] std::str::Utf8Error),
+    #[error("Xterm-query error: {0}")]
+    XtermQuery(#[from] xterm_query::XQError),
     #[error("Parse Int error: {0}")]
     ParseInt(#[from] std::num::ParseIntError),
     #[error("Wrong answer format: {0}")]
     WrongFormat(String),
-    #[error("Timeout waiting for xterm")]
-    Timeout,
-    #[error("Terminal error code: {0}")]
-    TerminalError(i64),
-    #[error("Nix error: {0}")]
-    #[cfg(unix)]
-    NixError(#[from] nix::errno::Errno),
-    //#[error("Nix error: {0}")]
-    //NixError(i32),
     #[error("No $COLORFGBG env variable")]
     NoColorFgBgEnv,
     #[error("Var error: {0}")]
--- src/xterm.rs
+++ src/xterm.rs
@@ -2,49 +2,15 @@ use {
     crate::*,
 };
 
-#[cfg(not(unix))]
-fn query(_query: &str, _response: &mut[u8], _timeout_ms: isize) -> Result<usize, TlError> {
-    Err(TlError::Unsupported)
-}
-
-/// query the xterm interface, assuming the terminal is in raw mode
-/// (or we would block waiting for a newline)
 #[cfg(unix)]
-fn query_in_raw(query: &str, response: &mut[u8], timeout_ms: isize) -> Result<usize, TlError> {
-    use nix::sys::epoll::*;
-    use std::io::{self, Read, Write};
-    let stdin = io::stdin();
-    let mut stdin = stdin.lock();
-    let stdout = io::stdout();
-    let mut stdout = stdout.lock();
-    write!(stdout, "{}", query)?;
-    stdout.flush()?;
-    let poll_fd = epoll_create1(EpollCreateFlags::empty())?;
-    let mut event = EpollEvent::new(EpollFlags::EPOLLIN, 0);
-    epoll_ctl(
-        poll_fd,
-        EpollOp::EpollCtlAdd,
-        nix::libc::STDIN_FILENO,
-        Some(& mut event),
-    )?;
-    let mut events = [EpollEvent::empty(); 1];
-    let fd_count = epoll_wait(poll_fd, &mut events, timeout_ms)?;
-    if fd_count == 0 {
-        Err(TlError::Timeout) // no file descriptor was ready in time
-    } else {
-        let bytes_written = stdin.read(response)?;
-        Ok(bytes_written)
-    }
-}
-
-#[cfg(unix)]
-fn query(query: &str, response: &mut[u8], timeout_ms: isize) -> Result<usize, TlError> {
+fn query(query: &str, timeout_ms: isize) -> Result<String, TlError> {
     use crossterm::terminal::*;
     let switch_to_raw = !is_raw_mode_enabled()?;
     if switch_to_raw {
         enable_raw_mode()?;
     }
-    let res = query_in_raw(query, response, timeout_ms);
+    let res = xterm_query::query(query, timeout_ms)
+        .map_err(|e| e.into());
     if switch_to_raw {
         disable_raw_mode()?;
     }
@@ -59,9 +25,7 @@ pub fn query_bg_color() -> Result<Rgb, TlError> {
     // References:
     // - https://stackoverflow.com/a/28334701/263525
     // - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
-    let mut buffer = [0;50];
-    let n = query( "\x1b]11;?\x07", &mut buffer, 20)?;
-    let s = std::str::from_utf8(&buffer[..n])?;
+    let s = query( "\x1b]11;?\x07", 20)?;
     // the string we receive is quite strange.
     // For example, supposing the background is in #38A4C9 (blue),
     // then we receive "\u{1b}]11;rgb:3838/a4a4/c9c9\u{1b}\\"
